e89fc4458c268096692230e4f3935c7c20d1c9c6
付杰 <fujie@sinosoft.com.cn>
2020-03-22 17:26:43 +0800

    紫微文投

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*数字递增*/
+var options = {
+    useEasing: true,
+    useGrouping: true,
+    separator: ',',
+    decimal: '.',
+    prefix: '',
+    suffix: ''
+};
+
+/*首页搜索*/
+$(".search-box .text").click(function() {
+    $(".search-box .input").addClass("rinput");
+});
